c2c1adb23573ee3939b0c11f6dc2b19bc1de8dc1
azhermahmood08 <142876354+azhermahmood08@users.noreply.github.com>
2024-09-03 14:47:36 +0530

    first commit

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Navbar.css";
+import logo from "../../assets/logo.svg";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <img src={logo} alt="" />
+      <li>a</li>
+      <li>s</li>
+      <li>s</li>
+      <li>sd</li>
+    </div>
+  );
+};
+
+export default Navbar;
